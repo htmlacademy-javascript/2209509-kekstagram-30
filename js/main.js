0ddb3462c33a7efb7f180971a7df1b2aba7edb8d
darya <larisalebedeva209@gmail.com>
2023-11-12 06:39:03 +0000

    миниатюры(исправить)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 import {DESCRIPTION} from './constants.js';
 import {getRandomInteger} from './get-random-integer.js';
-import {createComment} from './create-comment.js';
+import {similarComment} from './create-comment.js';
+import {renderMiniature} from './miniature.js';
 
 const createID = () => {
   const randomIDIndex = getRandomInteger(0, 25);
   const randomAdressIndex = getRandomInteger(1, 25);
   const randomDescriptionIndex = getRandomInteger(0, DESCRIPTION.length - 1);
   const randomLikesIndex = getRandomInteger(1, 200);
-  const randomAvatarIndex = getRandomInteger(1, 6);
 
   return {
     id: randomIDIndex,
     url: String(`photos/${ randomAdressIndex }.jpg.`),
-    avatar: String(`img/avatar-${ randomAvatarIndex }.svg.`),
     description: DESCRIPTION[randomDescriptionIndex],
     likes: randomLikesIndex,
-    comment: createComment,
+    comment: similarComment,
   };
 };
+renderMiniature(createID());
 
-const similarCommentId = Array.from({length: 25}, createID);
-
-console.log(similarCommentId);
+export const similarCommentId = Array.from({length: 25}, createID);
